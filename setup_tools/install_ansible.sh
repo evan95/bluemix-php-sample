7f46d7dc6f915e9558affe850f27f68ed49eaa9c
Evandro Moraes <evandromoraes95@gmail.com>
2020-03-25 22:41:40 -0300

    Update install_ansible.sh

--- setup_tools/install_ansible.sh
+++ setup_tools/install_ansible.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "apt-get -qq -y install python3.6"
+sudo apt-get -qq -y install python3.6
 python --version
 echo "apt-get -qq update"
 sudo apt-get -qq update
-echo "apt-get -qq -y install python3 libssl-dev libffi-dev"
-sudo apt-get -qq -y install python3 python3-pip libssl-dev libffi-dev
+echo "apt-get -qq -y install python-dev libssl-dev libffi-dev"
+sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
 echo "pip install pycrypto pyyaml ansible --quiet"
 sudo pip install pycrypto pyyaml ansible --quiet
 
